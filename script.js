cf88a024b49d8da85eabb7186dd6db150672882f
kiyain12 <55067680+kiyain12@users.noreply.github.com>
2020-04-02 21:33:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+    $(".g-signin2").css("display", "none");
+    $(".data").css("display", "block");
+    $("#pic").attr('src',profile.getImageUrl());
+    $("#email").text(profile.getEmail());
+    
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+
+function signOut(){
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function()){
+                         
+                         alert("your account has been logged out");
+    $(".g-signin2").css("display","block");
+    $(".data").css("display","none");
+});
+}
